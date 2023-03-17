67d3b8f4220ccc40c8be18cee930bd5416b9a99d
zonin <business.kariukimartin@gmail.com>
2023-03-17 20:33:37 +0300

    bug file not saving

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <personal.h>
 #include <student.h>
 int main(){
-    Database<Personal>().run();
-    // Database<Student>().run();
+    // Database<Personal>().run();
+    Database<Student>().run();
     return 0;
 }
