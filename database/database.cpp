115cdde273ad5f88acb23ca655713d369269bb9d
zonin <business.kariukimartin@gmail.com>
2023-03-17 19:15:58 +0300

    add function defination

--- database/database.cpp
+++ database/database.cpp
@@ -8,3 +8,10 @@ Database<T>::Database(){
 
 }
 
+template<class T>
+void Database<T>::add(T &d){
+    database.open(fName, ios::in | ios::out | ios::binary);
+    database.seekp(0, ios::end);
+    d.writetoFile(database);
+    database.close();
+}
